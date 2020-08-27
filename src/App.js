357c5ba03b3c033c4202e2cb9e0c4352918e0ca2
Ahmed Bule <ahmedbule01@gmail.com>
2020-08-26 19:31:27 -0500

    added info on the app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import {BrowserRouter, Switch,Route} from "reac-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Aboutme from "./components/Aboutme";
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
+      <BrowserRouter>
+      <Switch>
+      <Route exact path ="/"><Aboutme /></Route>
+      <Route exact path ="/Aboutme"><Aboutme /></Route>
+      <Route exact path ="/portfolio"><Portfolio /></Route>
+      <Route exact path ="/contact"><Contact /></Route>
+      </Switch>
+      </BrowserRouter>
+       <Footer />
+     
     </div>
   );
 }
