20eebbae0867fc863c3bdd74025cfd2f6a655204
Ahmed Bule <ahmedbule01@gmail.com>
2020-09-17 18:01:15 -0500

    added apps in the json folder

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -11,10 +11,10 @@ class Portfolio extends React.Component{
             <div className="container">
         {this.state.apps.map((project,i) =>{
             return(<div className="card">
-            <h6>{project.title}</h6>
-           <a href={project.repo}>Github</a>
-            <a href={project.live}>Live</a>
-              <img src={project.img} width="300" height="300"/>
+            <h6>{project.appname}</h6>
+           <a href={project.repo} target="_blank" rel="noopener noreferrer">Check Github</a>
+            <a href={project.live} target="_blank" rel="noopener noreferrer" >Live</a>
+              <img src={project.img} width="200" height="200"/>
             </div>)
         })}
         </div>
