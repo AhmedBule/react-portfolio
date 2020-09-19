c2b9202e55447cf1f6a622ae867bec7d2090888c
Ahmed Bule <ahmedbule01@gmail.com>
2020-09-19 11:57:44 -0500

    Updates

---
+++ precache-manifest.931d5e802b95695a2ee22bf0d82e55f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dc1d691ac1868fb82ce81eec4a2a7d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e196e3c9dbacaf22a921",
+    "url": "/static/css/main.e393b47b.chunk.css"
+  },
+  {
+    "revision": "0b376269331e5b9ec4dd",
+    "url": "/static/js/2.a52f5bcc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a52f5bcc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e196e3c9dbacaf22a921",
+    "url": "/static/js/main.2fcc5225.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
